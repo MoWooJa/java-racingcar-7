5166be0cd0b8457f360f82305eb1be8d3f6f6917
kimsehw <seunghwan9703@gmail.com>
2024-11-26 17:02:41 +0900

    feat(Car): 자동차 전진 기능 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+public class Car {
+
+    public static final int MOVEABLE = 4;
+
+    private final String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= MOVEABLE) {
+            position++;
+        }
+    }
+}
