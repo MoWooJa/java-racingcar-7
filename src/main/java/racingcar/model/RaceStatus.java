6ce3f43068797b14dacefd27c788f8c599dc3a25
kimsehw <seunghwan9703@gmail.com>
2024-11-26 18:55:23 +0900

    feat(RacingJudge): 우승자 결정 기능 구현

--- src/main/java/racingcar/model/RaceStatus.java
+++ src/main/java/racingcar/model/RaceStatus.java
@@ -6,7 +6,9 @@ public class RaceStatus {
 
     public static final String CAR_STATUS_FORMAT = " : ";
     public static final String POSITION_MARKER = "-";
-    private final List<CarStatus> raceStatus;
+    public static final int MIN_POSITION = 0;
+
+    public final List<CarStatus> raceStatus;
 
     public RaceStatus(List<CarStatus> raceStatus) {
         this.raceStatus = raceStatus;
@@ -27,5 +29,36 @@ public class RaceStatus {
     private String getRoundPositionResult(CarStatus carStatus) {
         int position = carStatus.getPosition();
         return POSITION_MARKER.repeat(position);
+    }
+
+    public List<String> getLeaders() {
+        int leadPosition = getLeadPosition();
+        return raceStatus.stream()
+                .filter(carStatus -> carStatus.getPosition() == leadPosition)
+                .map(CarStatus::getName).toList();
+    }
+
+    private int getLeadPosition() {
+        int leadPosition = MIN_POSITION;
+        for (CarStatus status : raceStatus) {
+            leadPosition = getLeadPosition(status.getPosition(), leadPosition);
+        }
+        return leadPosition;
+    }
+
+    private int getLeadPosition(int position, int leadPosition) {
+        if (leadPosition < position) {
+            leadPosition = position;
+        }
+        return leadPosition;
+    }
+
+    public static void main(String[] args) {
+        RaceStatus test = new RaceStatus(List.of(new CarStatus("dd", 9),
+                new CarStatus("tt", 9), new CarStatus("ss", 8)));
+        System.out.println(test.getLeadPosition());
+        System.out.println(test.raceStatus.stream().filter(h -> h.getPosition() == test.getLeadPosition())
+                .map(CarStatus::getName).toList()
+                .toString());
     }
 }
---
+++ src/main/java/racingcar/model/RacingJudge.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class RacingJudge {
+    public List<String> getWinners(List<RaceStatus> raceResult) {
+        // 확장성,,? 일반성,,? 위해 RaceStatus는 winner를 특정 x 선두를 뽑도록
+        // judge에서는 결과의 마지막 뽑아서 우승자 뽑도록 특정함
+        RaceStatus lastRoundRace = raceResult.getLast();
+        return lastRoundRace.getLeaders();
+    }
+}
